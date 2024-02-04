1b98dbcb95aa6ad6bc073142c5aee54bccf30493
FireFox174 <158850415+FireFox174@users.noreply.github.com>
2024-02-04 13:22:14 -0600

    Update sprite.cs

--- sprite.cs
+++ sprite.cs
@@ -24,18 +24,6 @@ namespace MonoGame_Project1.Content.Sprites
                 return new Rectangle((int)Position.X, (int)Position.Y, image.Width, image.Height);
             }
         }
-
-        public virtual bool collsion(Rectangle rectangle1, Rectangle rectangle2)
-        {
-            if (rectangle1.Intersects(rectangle2))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
         public virtual void Update(GameTime gameTime)
         {
 
